829a6ce18b3f4301efbf44f01491c700d58b46bb
Guilherme <guisouzaskill@gmail.com>
2026-09-10 08:47:08 -0300

    test: add mocked unit tests for login screen component

---
+++ frontend/src/components/LoginScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * Contrato do componente LoginScreen (Skeleton para TDD)
+ * @param {Object} props
+ * @param {() => void} [props.onNavigateToDashboard] - Callback para navegação de retorno ao painel
+ */
+export default function LoginScreen({ onNavigateToDashboard }) {
+  return null;
+}
